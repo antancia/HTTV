2bfe878583c82d5a0f1ec5bffd00e0901145985b
Harmony <antancia@gmail.com>
2019-08-05 20:57:32 -0400

    example inline style

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const carouselStyle = {
+  color: "blue"
+};
+
 export default function Carousel() {
   return <div style={carouselStyle}>Carousel will go here!</div>;
 }
