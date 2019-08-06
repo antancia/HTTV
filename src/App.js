8f939f2b14bdae0e8edbfadeda598442b9d6ada5
Harmony <antancia@gmail.com>
2019-08-05 20:55:53 -0400

    boilerplate carousel component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Carousel from "./components/Carousel";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Burrito</p>
+      <p>HTTV</p>
+      <div>Featured movie content!</div>
+      <Carousel />
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Carousel() {
+  return <div style={carouselStyle}>Carousel will go here!</div>;
+}
