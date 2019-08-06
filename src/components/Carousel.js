f84a2463824efe606aa7941f116d1b419fbc0814
tessa <tessaSAC@users.noreply.github.com>
2019-08-05 21:02:45 -0400

    adds css but who knows if it works???

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-const carouselStyle = {
-  color: "blue"
-};
+const styleCarousel = {
+  background: 'white',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+}
+
+const stylePleb = {
+  background: 'black',
+  width: '16vw',
+  height: '9vw',
+}
+
+const styleTheOne = {
+  background: 'red',
+  width: '32vw',
+  height: '18vw',
+}
+
 
 export default function Carousel() {
-  return <div style={carouselStyle}>Carousel will go here!</div>;
+  return <div style={ styleCarousel }>
+    <div style={ stylePleb } />
+    <div style={ styleTheOne } />
+  </div>;
 }
