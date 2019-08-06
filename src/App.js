01c2190751aeb9912834161fa3e3644fc5e8aa8c
Harmony <antancia@gmail.com>
2019-08-05 21:36:10 -0400

    syntax fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Carousel from "./components/Carousel";
 import "./App.css";
 
+const url = "";
+
 function App() {
   return (
     <div className="App">
       <p>HTTV</p>
       <div>Featured movie content!</div>
-      <audio autoplay loop>
-        <source src="{ url }" type="audio/ogg" />
+      <audio autoPlay loop>
+        <source src={url} type="audio/ogg" />
         Your browser does not support the <code>audio</code> element.
       </audio>
       <Carousel />
